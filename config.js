2c6fddd5b9b78c283ae4432bcef8e6114c39619d
portal-rh-cmivet <cmivetrh@gmail.com>
2026-07-21 13:32:46 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG={
+API_URL:"https://script.google.com/macros/s/AKfycbwDhEHF1V-pXeV6KLH-3EoYDx1IEyHS-MtFsmgp1IbkFeoRr2hdTzbe-r4oStxdnNhK/exec",
+VERSION:"6.0",
+STORAGE_TOKEN:"cmivet_token",
+STORAGE_USER:"cmivet_usuario"
+};
