10ce9589464083e8089ac5bda100cfa567f509f9
portal-rh-cmivet <cmivetrh@gmail.com>
2026-07-21 13:52:24 -0300

    Atualiza config.js para integração com Apps Script

--- config.js
+++ config.js
@@ -1,6 +1,20 @@
-const CONFIG={
-API_URL:"https://script.google.com/macros/s/AKfycbwDhEHF1V-pXeV6KLH-3EoYDx1IEyHS-MtFsmgp1IbkFeoRr2hdTzbe-r4oStxdnNhK/exec",
-VERSION:"6.0",
-STORAGE_TOKEN:"cmivet_token",
-STORAGE_USER:"cmivet_usuario"
+const CONFIG = {
+    APP_NAME: "Portal RH CMIVET",
+    VERSION: "6.0",
+
+    API_URL:
+        "https://script.google.com/macros/s/AKfycbwDhEHF1V-pXeV6KLH-3EoYDx1IEyHS-MtFsmgp1IbkFeoRr2hdTzbe-r4oStxdnNhK/exec",
+
+    STORAGE: {
+        TOKEN: "cmivet_token",
+        USER: "cmivet_user",
+        EMAIL: "cmivet_email"
+    },
+
+    REQUEST_TIMEOUT: 30000,
+
+    DEBUG: true
 };
+
+// Evita alterações acidentais na configuração
+Object.freeze(CONFIG);
