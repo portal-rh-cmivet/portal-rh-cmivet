c89d0224e801313cc0b9ce777cce1f422c412351
portal-rh-cmivet <cmivetrh@gmail.com>
2026-07-24 11:15:19 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -19,8 +19,8 @@ const API = {
 
                 method: "POST",
 
-                headers: {
-                    "Content-Type": "application/json"
+               headers: {
+    "Content-Type": "text/plain;charset=utf-8"
                 },
 
                 body: JSON.stringify({
